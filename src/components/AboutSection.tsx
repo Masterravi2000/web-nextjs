5af6f977e45cb95d17f749360671ee2d7b35af2b
Masterravi2000 <raviqazqwerty123456@gmail.com>
2025-10-02 23:40:17 +0530

    Refactored

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 const AboutSection = () => {
   return (
     <div className='w-full bg-black pt-4'>
-      <div className='flex flex-row  w-full'>
-      <div className='w-[60%] flex justify-center p-8 py-20 border-[#181818] border-b-1 border-t-1 border-r-1'>
+      <div className='flex flex-row w-full'>
+      <div className='w-[40%] flex justify-center p-8 py-20 border-[#181818] border-b-1 border-t-1 border-r-1'>
         <a className='text-white text-6xl font-semi-bold'>
-          WHY PARTNER WITH ME?
+          About Me?
           </a>
       </div>
-      <div className='w-[40%] border-[#181818] border'/>
+      <div className='w-[60%] border-[#181818] border'/>
       </div>
       <div className='w-full border border-[#181818] justify-center flex p-8'>
         <div className='w-[80%]'>
         <a className='text-white text-2xl font-bold'>
-          With 6+ years Webflow expertise, I combine technical precision, a design-led mindset, and immersive animation to craft high-impact web experiences that help teams move faster, collaborate freely, and scale with fewer technological constransts
+          Software Engineer skilled in React Native (Expo & Bare), React.js, TypeScript, Node.js, JavaScript, Redux Toolkit, and MERN stack. Experienced in building scalable, high-performance applications with AWS and Docker, reusable architectures, CI/CD workflows, and optimized systems for speed, reliability, and seamless user experiences.
           </a>
           </div>
       </div>
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -5,9 +5,24 @@ import ProjectCard from './Cards/ProjectCards';
 const ProjectSection = () => {
 
     const items = [
-        "This is a standard project am writing this to test and check what happening here",
-        "This is a standard project am writing this to test and check what happening here",
-        "This is a standard project am writing this to test and check what happening here",
+        "Led the end-to-end development of a sports-centric social platform, architected to handle 10,000+ concurrent users and a total capacity of 200,000 users, ensuring scalability, high availability, and fault tolerance.",
+        "Stamin is an AI-driven health, fitness, and sports platform that enables users to build and maintain a comprehensive personal wellness profile. By integrating with smartwatches and allowing manual data entry, Stamin records essential metrics such as time, distance, heart rate, and medical reports.",
+        "Led the end-to-end development of Qilin, a Gen-Z second-hand fashion marketplace in India, engineered for scalability, security, and responsiveness while ensuring seamless UI/UX, reusability, and a sustainable user-centric shopping experience.",
+    ];
+
+    const project = [
+        {
+            text: "This is the first project, testing dynamic images.",
+            image: "/StrengthLogo.png",
+        },
+        {
+            text: "This is the second project, showing a different logo.",
+            image: "/StaminLogo.png",
+        },
+        {
+            text: "This is the third project, with yet another image.",
+            image: "/QilinLogo.png",
+        },
     ];
 
     return (
@@ -15,7 +30,7 @@ const ProjectSection = () => {
             <a className='text-6xl font-semi-bold text-white p-10'>Projects</a>
             <div className='flex flex-col md:flex-row w-full gap-5 sm:px-10 pt-10'>
                 {items.map((text, index) => (
-                    <ProjectCard key={index} text={text} />
+                    <ProjectCard key={index} text={text} image={project[index].image} />
                 ))}
             </div>
         </div>
